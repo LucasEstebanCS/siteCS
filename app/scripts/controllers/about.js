4596305c351ad9c0ea5a2b224010dd5aa1a5f879
Lucas <l.esteban@chequesante.com>
2015-07-02 14:51:05 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name siteCsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the siteCsApp
+ */
+angular.module('siteCsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name siteCsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the siteCsApp
+ */
+angular.module('siteCsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
